04e5a2cd446497507bda76f25ff7e34d8a827579
VDNgo <ngovietdung@thomsonmedical.com>
2024-08-08 15:57:07 +0700

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,42 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  preview: {
-    port: 3001,
-  },
-  // for dev
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+;
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return defineConfig({
+    plugins: [react(), viteTsconfigPaths()],
+    preview: {
+      port: 3001,
     },
-  },
-});
+    // for dev
+    server: {
+      port: 3000,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    define: {
+      // here is the main update
+      global: "globalThis",
+      "process.env": env,
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              if (id.includes('@react-pdf-viewer')) {
+                return 'react-pdf-viewer';
+              }
+              return id.toString().split("node_modules/")[1].split("/")[0].toString();
+            }
+          },
+        },
+      },
+    },
+  })
+};
